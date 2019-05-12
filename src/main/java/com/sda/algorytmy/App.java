a238454e2a430fee59e08d3ec37c88f91ff7d55a
lukaszog <lukasz.ogan@gmail.com>
2019-05-12 09:57:16 +0100

    add sequence solution

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        int tab[] = new int[20];
+        tab[0] = 0;
+        tab[1] = 1;
+        for (int i=2; i<tab.length; i++){
+            tab[i] = tab[i-1] + tab[i-2];
+            System.out.println(tab[i]);
+        }
     }
 }
